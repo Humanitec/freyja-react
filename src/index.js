e116dcee8e68d881d2260a64cdf1ea1c1db0b5aa
mughees <mugheesilyas@live.com>
2019-06-27 02:34:25 +0200

    themeing done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./index.css";
+import { createTheme } from 'theme'
+import { ThemeProvider } from "styled-components";
+
+const themeColors = {
+    primary: '#ddd009',
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+/*ReactDOM.render(
+    <ThemeProvider theme={createTheme(themeColors)}  >
+        <App />
+    </ThemeProvider>,
+    document.getElementById("root")
+);*/
--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { FjCard } from 'freyja/Card';
 import Highlight from 'react-highlight';
+import {createTheme} from "theme";
+import { ThemeProvider } from "styled-components";
+
+const themeColors = {
+    lightGray: '#aa0094',
+    white: '#ff1a29',
+    text: '#ffffff'
+};
 
 function CardPage() {
   return (
@@ -10,6 +18,12 @@ function CardPage() {
         content="This is the card body!">
         This is the card header!
       </FjCard>
+        <ThemeProvider theme={createTheme(themeColors)}  >
+            <FjCard
+                content="This is the card body!">
+                This is the card header!
+            </FjCard>
+        </ThemeProvider>
       <Highlight language="javascript">
 {
 `<FjCard
