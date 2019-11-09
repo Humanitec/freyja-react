8d6310889f609730e7139c9b1bb06ce52d1bf12b
Andrew Shortall <dyallsho@icloud.com>
2019-11-09 03:45:42 +0100

    Added content switcher keys

--- src/pages/Button.js
+++ src/pages/Button.js
@@ -6,17 +6,16 @@ import { ThemeProvider } from "styled-components";
 import { withCustomTheme } from 'freyja/custom-theme/custom-theme'
 
 
-
 const themeColors = {
-    primary: '#006385',
-    danger: '#aa7226',
+  primary: '#006385',
+  danger: '#aa7226',
 };
 
 const MyNewThemedButton = withCustomTheme(FjButton, createTheme(themeColors));
 
 function ButtonPage() {
   return (
-    <div className="page-content">
+    <div className="page-content element-wrapper">
       <h3>Button</h3>
       <h4>Variants</h4>
       <FjButton>Primary</FjButton>
@@ -27,17 +26,13 @@ function ButtonPage() {
             primary: '#1ddd28',
             danger: '#aa0094',
         })}  >
-            <div>
-                <div>
-                    <FjButton>themed primary</FjButton>
-                </div>
-                <div>
-                    <FjButton variant="danger">Themed Danger</FjButton>
-                </div>
-            </div>
+          <div className="element-wrapper">
+            <FjButton>Themed Primary</FjButton>
+            <FjButton variant="danger">Themed Danger</FjButton>
+          </div>
         </ThemeProvider>
 
-        <MyNewThemedButton> custom buttom </MyNewThemedButton>
+        <MyNewThemedButton>Custom Button</MyNewThemedButton>
       <Highlight language="javascript">
 {
 `<FjButton>Primary</FjButton>
@@ -45,18 +40,16 @@ function ButtonPage() {
 <FjButton variant="danger">Danger</FjButton>
 
 you can also use themeProvider to pass your own theme to button.
-<ThemeProvider theme={themeColors}  >
-    <div>
-        <FjButton>themed primary</FjButton>
-        <FjButton variant="danger">Themed Danger</FjButton>
-    </div>
+<ThemeProvider theme={themeColors}>
+  <FjButton>Themed Primary</FjButton>
+  <FjButton variant="danger">Themed Danger</FjButton>
 </ThemeProvider>
 
 
 you can also generate button with your own style using our pre-made button
 
 const MyNewThemedButton = withCustomTheme(FjButton, createTheme(themeColors));
-<MyNewThemedButton> custom buttom </MyNewThemedButton>
+<MyNewThemedButton>Custom Button</MyNewThemedButton>
 
 `
 }
