f37a69624fc509dba6068c256be700a0fb765b97
Andrew Shortall <dyallsho@icloud.com>
2019-11-09 14:20:58 +0100

    Removed highlight from non-expandable card

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -14,22 +14,47 @@ function CardPage() {
   return (
     <div className="page-content">
       <h3>Card</h3>
+      <h4>Expandable</h4>
       <FjCard
         content="This is the card body!">
         This is the card header!
       </FjCard>
-        <ThemeProvider theme={createTheme(themeColors)}  >
-            <FjCard
-                content="This is the card body!">
-                This is the card header!
-            </FjCard>
-        </ThemeProvider>
       <Highlight language="javascript">
 {
 `<FjCard
   content="This is the card body!">
   This is the card header!
 </FjCard>`
+}
+      </Highlight>
+
+      <h4>Non-expandable</h4>
+      <FjCard>
+        This card has no body!
+      </FjCard>
+      <Highlight language="javascript">
+{
+`<FjCard>
+  This card has no body!
+</FjCard>`
+}
+      </Highlight>
+
+      <h4>Themed</h4>
+      <ThemeProvider theme={createTheme(themeColors)}>
+        <FjCard
+          content="This is the card body!">
+          This is the card header!
+        </FjCard>
+      </ThemeProvider>
+      <Highlight language="javascript">
+{
+`<ThemeProvider theme={createTheme(themeColors)}>
+  <FjCard
+    content="This is the card body!">
+    This is the card header!
+  </FjCard>
+</ThemeProvider>`
 }
       </Highlight>
     </div>
